c0a8b41b9bcb54c8b912c23ee733ab5016d28d2a
Jorge Blanco <jbsfreelance@gmail.com>
2020-06-16 22:08:39 -0700

    Added a basic room level and a door opener script

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,37 @@
+// Copyright Jorge Blanco 2020
+
+
+#include "OpenDoor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	const float TargetRotation = 90.f;
+	FRotator CurrentRotation = GetOwner()->GetActorRotation();
+	CurrentRotation.Add(0.f, TargetRotation, 0.f);
+	GetOwner()->SetActorRotation(CurrentRotation);
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
